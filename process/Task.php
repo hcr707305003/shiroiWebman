111de4ddd6df9906d144cf1aae2e4120177a79ec
hcr707305003 <707305003@qq.com>
2024-04-09 12:31:04 +0800

    V0.0.1 新增控制台输入二维码

--- process/Task.php
+++ process/Task.php
@@ -5,13 +5,32 @@ namespace process;
 use support\Db;
 use Workerman\Crontab\Crontab;
 
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelLow;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\ConsoleWriter;
+
 class Task
 {
     public function onWorkerStart()
     {
+        $data = 'https://www.baidu.com';
+
+        //控制台生成二维码
+        echo(Builder::create()
+            ->encoding(new Encoding('UTF-8'))
+            ->errorCorrectionLevel(new ErrorCorrectionLevelLow())
+            ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+            ->writer(new ConsoleWriter())
+            ->data($data)
+            ->build()
+            ->getString());
+
+
         //每个一分钟执行一下查库动作，防止mysql go away
-        new Crontab('0 */5 * * * *', function(){
-            dump(console(to_array(Db::select("show processlist"))));
-        });
+//        new Crontab('0 */5 * * * *', function(){
+//            dump(console(to_array(Db::select("show processlist"))));
+//        });
     }
 }
